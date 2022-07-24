1996e59f66748ef2b869277763925bc97d4658a5
Matheus Prudente <maths.henrique.14.2001@gmail.com>
2022-07-24 16:15:33 -0300

    Modulo dois

--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -1,10 +1,40 @@
 package com.algaworks.algalog.domain.model;
 
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "TB_CLIENTE")
 public class Cliente {
 
+	@Id
+	@Column(name = "ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@NotBlank
+	@Size(max = 60)
+	@Column(name = "NOME")
 	private String nome;
+
+	@NotBlank
+	@Email
+	@Size(max = 255)
+	@Column(name = "EMAIL")
 	private String email;
+
+	@NotBlank
+	@Size(max = 20)
+	@Column(name = "FONE")
 	private String telefone;
 
 	public Cliente() {
@@ -49,6 +79,23 @@ public class Cliente {
 
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
 	}
 
 }
---
+++ src/main/resources/db/migration/V0002__renomea-coluna-telefone.sql
@@ -0,0 +1,1 @@
+alter table tb_cliente rename column telefone to fone;
